3a323668455126a0f096759f4053d942243c7b7a
Koorosh <Kooroshfarahani3@gmail.com>
2025-08-03 23:06:40 +0330

    update loger

--- 12_w_praktis/simple_logger.py
+++ 12_w_praktis/simple_logger.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-with open("log.txt","w") as file:
+with open("log.txt","a") as file:
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     file.write(f"\n{now}\n")
 
