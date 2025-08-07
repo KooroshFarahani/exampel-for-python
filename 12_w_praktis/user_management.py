bd1c313189f45c2b85eeff1ee66a930d1bd1b4d3
Koorosh <Kooroshfarahani3@gmail.com>
2025-08-07 16:35:28 +0330

    update log user and check it

--- 12_w_praktis/user_management.py
+++ 12_w_praktis/user_management.py
@@ -12,13 +12,16 @@ class user():
     def login(self):
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S") # get date and time 
         print(f" user name :{self.username} login at {now} welcome ")
-        data = f" username : {self.username} \t email : {self.email} \t password : {self.password} \t admin : {self.admin} \n"
+        data = f" username : {self.username} \t email : {self.email} \t password : {self.password} \t admin : {self.admin} \t {now} \n"
 
         with open('log-user.txt','a') as file: # open file to write log login
             file.write(data)
+    @staticmethod
+    def check_user(user_user):
+        
         with open ('log-user.txt','r') as file:
             read=file.read()
-            if self.username in read:
+            if user_user in read:
                 print ("user name is exist")
             else :
                 print ("user name in not exist") 
@@ -33,12 +36,13 @@ class user():
 
 
 txt="1.login \n 2.sining \n 3.show user"
-
-username = input("enter username :")
-password = input("enter password :")
-email = input("enter the email :")
-admin=bool(input("True or False for admin"))
+print(txt)
+user_user = input("enter username :")
+#password = input("enter password :")
+#email = input("enter the email :")
+#admin=bool(input("True or False for admin"))
     
-user1=user(username,email,password,admin)
+#user1=user(username,email,password,admin)
 
-user1.login()
+#user1.login()
+user.check_user(user_user)
