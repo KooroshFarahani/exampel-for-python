6cf75e7285335f4e04f2b055f877c560b54e493c
koorosh <kooroshfarahani3@gmail.com>
2025-08-09 02:36:03 +0330

    ساخت یک گیم ساده

---
+++ 12_w_praktis/game_oop.py
@@ -0,0 +1,35 @@
+class Character:
+    def __init__(self, health, attack, name):
+        self.health = health
+        self.attack = attack
+        self.name = name
+
+    def fight(self, opponent):
+        """این شخصیت به دیگری حمله می‌کند"""
+        print(f"{self.name} attacks {opponent.name} with {self.attack} damage!")
+        opponent.health -= self.attack
+        if opponent.health <= 0:
+            print(f"{opponent.name} is defeated!")
+        else:
+            print(f"{opponent.name} has {opponent.health} health left.")
+
+
+class Warrior(Character):
+    def __init__(self):
+        # مقداردهی اولیه برای واریر
+        super().__init__(health=150, attack=25, name="Warrior")
+
+
+class Mage(Character):
+    def __init__(self):
+        # مقداردهی اولیه برای مجی
+        super().__init__(health=100, attack=40, name="Mage")
+
+# ساخت دو شخصیت
+warrior = Warrior()
+mage = Mage()
+
+# شروع نبرد!
+warrior.fight(mage)
+mage.fight(warrior)
+warrior.fight(mage)
