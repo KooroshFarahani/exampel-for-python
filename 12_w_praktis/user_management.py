f3cd8078716822ca22233ee39b117be5bd880e57
koorosh <kooroshfarahani3@gmail.com>
2025-08-09 01:50:49 +0330

    update login and make singin method

--- 12_w_praktis/user_management.py
+++ 12_w_praktis/user_management.py
@@ -3,31 +3,31 @@ import os
 from datetime import datetime
 class user():
     def __init__(self,username,email,password,admin=False):
+       
         self.username=username
         self.email=email
         self.password=password
         self.admin=admin
         
 
-    def login(self):
+    def singin(self):
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S") # get date and time 
-        print(f" user name :{self.username} login at {now} welcome ")
+        print(f" user name :{self.username} singin at {now} welcome ")
         data = f" username : {self.username} \t email : {self.email} \t password : {self.password} \t admin : {self.admin} \t {now} \n"
 
         with open('log-user.txt','a') as file: # open file to write log login
             file.write(data)
     @staticmethod
-    def check_user(user_user):
-        
+    def login (user_user,pass_pass):
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         with open ('log-user.txt','r') as file:
             read=file.read()
-            if user_user in read:
-                print ("user name is exist")
-            else :
+            if user_user in read and pass_pass in read :
+                print(f" user name :{user_user} login at {now} welcome ")
+                return True
+            else:
                 print ("user name in not exist") 
-            
-    def singin(self):
-        pass
+                return False
     def AdminUser(self):
         pass
 
@@ -35,14 +35,25 @@ class user():
         pass
 
 
-txt="1.login \n 2.sining \n 3.show user"
+txt="1.singin \n 2.login \n 3.show user"
 print(txt)
-user_user = input("enter username :")
-#password = input("enter password :")
-#email = input("enter the email :")
-#admin=bool(input("True or False for admin"))
-    
-#user1=user(username,email,password,admin)
 
-#user1.login()
-user.check_user(user_user)
+operator=input("enter the chose :")
+
+if operator=="1":
+    username=input("Please Enter the Username :")
+    email=input("Please Enter the Email :")
+    password=input("Please Enter the Password :")
+    admin=bool(input("Please Enter the Admin (True or False) :"))
+    user_create = user(username,email,password,admin)
+    user_create.singin()
+
+elif operator=="2":
+    username=input("Please Enter the Username :")
+    password=input("Please Enter the Password :")
+    if user.login(username,password):
+        print("\t")
+    else:
+        user.login(username,password)
+else:
+    pass
