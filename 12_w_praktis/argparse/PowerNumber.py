8502218be4531fbaef16e9fb8dd7f6c00b08bd8c
Koorosh <Kooroshfarahani3@gmail.com>
2025-08-25 12:42:06 +0330

    update power number

--- 12_w_praktis/argparse/PowerNumber.py
+++ 12_w_praktis/argparse/PowerNumber.py
@@ -2,15 +2,17 @@ import argparse
 def PowerNumber():
     try:
         parser=argparse.ArgumentParser(description="Calculate power of a number")
-        parser.add_argument("base",type=float,help="Base number")
-        parser.add_argument("exponent",type=float,help="Exponent number")
+        parser.add_argument("b","base",type=float,help="Base number")
+        parser.add_argument("e","exponent",type=float,help="Exponent number")
         parser.add_argument("-v","--verbos",action="store_true",help="Show verbose output")
+        parser.add_argument("-r","--round",action="store_true",type=int,help="Number of decimals",default=2)
 
         arg=parser.parse_args()
 
         result=arg.base**arg.exponent
 
         if arg.verbos:
+
             print(f"Calculating {arg.base} raised to the power of {arg.exponent}...")
             print(f"Result: {result}")
         else:
